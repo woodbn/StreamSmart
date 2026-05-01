11c0ce88fe56f7c3b347eafd311eda2ac760e342
woodbn <123123157+woodbn@users.noreply.github.com>
2026-04-30 23:34:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/StreamSmart/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
